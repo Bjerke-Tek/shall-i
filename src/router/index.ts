84aa6cc5f6d665c1c0587836f6dac8a5229f4fae
Erik Bjerke <contact@erikbjerke.com>
2021-12-14 01:24:18 +0000

    fix: prettier shit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import { RouteRecordRaw } from "vue-router";
+import Home from "../views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
   // {
@@ -8,15 +8,15 @@ const routes: Array<RouteRecordRaw> = [
   //   redirect: '/home'
   // },
   {
-    path: '/',
-    name: 'Shall you?',
-    component: Home
-  }
-]
+    path: "/",
+    name: "Shall you?",
+    component: Home,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
